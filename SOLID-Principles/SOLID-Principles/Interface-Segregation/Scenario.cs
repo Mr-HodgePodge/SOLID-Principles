11ab3aafb8c5bc0e7a301bc4c7e3495c0323e9b9
Dylan <dylan_krajnc@tutanota.com>
2023-09-10 23:50:13 +0200

    - added print example to demonstrate the Interface-Segregation Principle.

---
+++ SOLID-Principles/SOLID-Principles/Interface-Segregation/Scenario.cs
@@ -0,0 +1,52 @@
+﻿namespace SOLID_Principles.Interface_Segregation;
+
+public class Scenario
+{
+    public class Document
+    {
+        
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MultifunctionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //exists
+        }
+
+        public void Scan(Document d)
+        {
+            //exists
+        }
+
+        public void Fax(Document d)
+        {
+            //exists
+        }
+    }
+
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //exists
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
