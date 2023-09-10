0079f501a777fcbc836920d04dd991dc0583f83d
Dylan <dylan_krajnc@tutanota.com>
2023-09-10 15:50:29 +0200

    - initial project setup - added journal example demonstrating the need for the single responsibility principle

---
+++ SOLID-Principles/SOLID-Principles/Single-Responsibility/Solution.cs
@@ -0,0 +1,47 @@
+﻿namespace SOLID_Principles.Single_Responsibility;
+
+public class Solution
+{
+    public class Journal
+    {
+        private readonly List<String> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; //memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, journal.ToString());
+            }
+        }
+
+        public static Journal? Load(string filename)
+        {
+            return null;
+        }
+
+        public void Load(Uri uri)
+        {
+            
+        }
+    }
+}
